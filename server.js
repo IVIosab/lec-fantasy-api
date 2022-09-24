080dd6fc86b4a45bbc42f2dc408ccd7900fcae24
IVIosab <mosab.f.r@gmail.com>
2022-09-24 04:12:17 +0300

    fix: trying to fix cors problem

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 const playersRouter = require('./routes/players')
 app.use('/players', playersRouter)
 
