f4bde710184cc9a5465f280b3a2cb2c7756d8726
IVIosab <mosab.f.r@gmail.com>
2022-09-24 03:36:12 +0300

    fix: app headers domain

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
